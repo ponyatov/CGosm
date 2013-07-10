6f3f6fef58c8751a93fd350dfc40fdc603aa0540
Dmitry Ponyatov <dponyatov@gmail.com>
2013-07-10 11:51:18 +0400

    + py downloader
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- osm/osm_download.py
+++ osm/osm_download.py
@@ -1,5 +1,8 @@
+API_PROD = 'http://api.openstreetmap.org/api'
+API_DEV  = 'http://api06.dev.openstreetmap.org/api'
+API = API_DEV
+
 import os,sys,time
+C='wget -O %s %s/%s'%('capabilities',API,'capabilities') ; print C,;print os.system(C)
+print '.'
 
-
-
-raw_input('.')
